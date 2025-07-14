6fadac42664dfa5502610c13bbf6f6c5dc1576e1
Donald_Edwin <donaldedu172@gmail.com>
2025-07-14 03:36:26 +0300

    base url for https render

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,8 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Inertia\Inertia;
+use Illuminate\Support\Facades\URL;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,7 +21,12 @@ class AppServiceProvider extends ServiceProvider
      * Bootstrap any application services.
      */
     public function boot(): void
-{
-    Inertia::share('auth.user', fn () => Auth::user());
-}
+    {
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
+
+        Inertia::share('auth.user', fn() => Auth::user());
+    }
 }
